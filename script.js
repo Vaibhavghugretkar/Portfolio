08ca697383278978835ec0a32193914a3b70f10f
Vaibhav GH <vaibhavghugretkar23@gmail.com>
2024-01-14 14:18:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var typed=new Typed(".multiple-text",{
+    strings:['Front-end Developer','Content Writer','Cosmonaut'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})  
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+
+// //scroll active colours
+let secu=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header nav a');
+
+window.onscroll= () =>{
+      secu.forEach(sec=>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop -150 ;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top >=offset && top<offset+height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    let header=document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY>100);
+
+/* *********************************remove toggle and navbar when click */
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+};
+
